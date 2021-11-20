9a647e6bbb838be1cda5bf3115d90c0368099b7d
Monzikov Maxim <monzikovm@yandex.ru>
2021-11-20 16:53:35 +0300

    fix: remove eslint-double-line

--- models/user.js
+++ models/user.js
@@ -26,20 +26,18 @@ const userSchema = new mongoose.Schema({
   },
 }, { versionKey: false });
 
-userSchema.statics.findUserByCredentials = function ({ email, password }) {
-  return this.findOne({ email }).select('+password')
-    .then((user) => {
-      if (!user) {
-        throw new UnauthorizedError('Неправильные почта или пароль');
-      }
-      return bcrypt.compare(password, user.password)
-        .then((matched) => {
-          if (!matched) {
-            throw new UnauthorizedError('Неправильные почта или пароль');
-          }
-          return user;
-        });
-    });
-};
+userSchema.statics.findUserByCredentials = ({ email, password }) => this.findOne({ email }).select('+password')
+  .then((user) => {
+    if (!user) {
+      throw new UnauthorizedError('Неправильные почта или пароль');
+    }
+    return bcrypt.compare(password, user.password)
+      .then((matched) => {
+        if (!matched) {
+          throw new UnauthorizedError('Неправильные почта или пароль');
+        }
+        return user;
+      });
+  });
 
 module.exports = mongoose.model('user', userSchema);
