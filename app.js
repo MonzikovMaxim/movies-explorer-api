9dbc2099abee4bb0bc09825457f93ca1085f06c0
Monzikov Maxim <monzikovm@yandex.ru>
2021-11-22 20:58:26 +0300

    feat: add limiter, helmet

--- app.js
+++ app.js
@@ -1,17 +1,21 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
+const helmet = require('helmet');
 const { errors } = require('celebrate');
 const error = require('./middlewares/error');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 const routes = require('./routes');
+const limiter = require('./middlewares/limiter');
 
 const { PORT = 3000 } = process.env;
 const { MONGO_URL = 'mongodb://localhost:27017/moviesdb' } = process.env;
 const app = express();
-app.use(express.json());
 
 app.use(requestLogger);
+app.use(limiter);
+app.use(helmet());
+app.use(express.json());
 app.use(routes);
 app.use(errorLogger);
 app.use(errors());
---
+++ middlewares/limiter.js
@@ -0,0 +1,8 @@
+const rateLimit = require('express-rate-limit');
+
+module.exports = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100,
+});
+
+module.exports = rateLimit;
